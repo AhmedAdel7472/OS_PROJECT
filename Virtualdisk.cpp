0b4b67cbde033149737c61412e9db2cf14771f5f
Ahmed Adel <ahmed.adel201364@yahoo.com>
2024-10-28 17:48:26 +0300

    writing the classes

--- Virtualdisk.cpp
+++ Virtualdisk.cpp
@@ -6,23 +6,46 @@
 using namespace std;
 
 fstream Virtualdisk::disk;
-    
-void Virtualdisk::openorcreate(string path)
-{
-    
+
+// Function to open or create the virtual disk file
+void Virtualdisk::openorcreate(string path) {
+    // Open the disk file in read/write mode, creating it if it doesn't exist
+    disk.open(path, ios::in | ios::out | ios::binary);
+
+    // If the file doesn't exist, create it
+    if (!disk) {
+        // Create a new file
+        disk.open(path, ios::out | ios::binary);
+        disk.close();
+        // Reopen it in read/write mode
+        disk.open(path, ios::in | ios::out | ios::binary);
+    }
 }
-    
-void Virtualdisk::readcluster(vector<char> cluster,int cluster_index)
-{
 
+// Function to read a cluster of data from the virtual disk
+void Virtualdisk::readcluster(vector<char> cluster, int cluster_index) {
+    int cluster_size = cluster.size();
+    // Seek to the beginning of the desired cluster
+    disk.seekg(cluster_index * cluster_size, ios::beg);
+
+    // Read the data into the cluster vector
+    disk.read(&cluster[0], cluster_size);
 }
 
-char * Virtualdisk::writecluster(int cluster_index)
-{
+// Function to write a cluster of data to the virtual disk
+char * Virtualdisk::writecluster(int cluster_index) {
+    static char data[512]; // Assuming a cluster size of 512 bytes
 
+    // Seek to the correct cluster index position
+    disk.seekp(cluster_index * sizeof(data), ios::beg);
+
+    // Write the data to the disk at the specified position
+    disk.write(data, sizeof(data));
+
+    return data;
 }
 
-bool Virtualdisk::isNew()
-{
-
+// Function to check if the disk file is new
+bool Virtualdisk::isNew() {
+    return !disk.is_open();
 }
